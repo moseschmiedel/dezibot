3c72c5f772eea29e6a9e44f28b2643b53ef24cfd
bruderj15 <julian.bruder@outlook.com>
2025-03-05 13:54:46 +0100

    autocharge/#10-pick: specify `AbstractSet`

---
+++ src/autocharge/master/abstractSet/AbstractSet.hpp
@@ -0,0 +1,14 @@
+#ifndef ABSTRACTSET_H
+#define ABSTRACTSET_H
+
+#include <optional>
+
+template <typename T> class AbstractSet {
+public:
+  virtual void insert(T item) = 0;
+  virtual std::optional<T> pick() = 0;
+  virtual bool isEmpty() const = 0;
+  virtual ~AbstractSet() = default;
+};
+
+#endif // ABSTRACTSET_H
