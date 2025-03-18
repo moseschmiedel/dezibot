2f10ed5467eef1456d492e6c0fc4755c998b331c
bruderj15 <julian.bruder@outlook.com>
2025-03-18 13:47:55 +0100

    remove `AbstractSet::getId`

--- src/autocharge/master/abstractSet/AbstractSet.hpp
+++ src/autocharge/master/abstractSet/AbstractSet.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
-template <typename T, typename ID> class AbstractSet {
+template <typename T> class AbstractSet {
 public:
-  virtual void insert(ID id, T& item) = 0;
+  virtual void insert(T& item) = 0;
   virtual T* pick() = 0;
-  virtual T* get(ID id) = 0;
   virtual bool isEmpty() const = 0;
   virtual ~AbstractSet() = default;
 };
