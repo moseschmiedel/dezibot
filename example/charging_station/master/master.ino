3e238416d4170d83ada80e0a015147ef31507579
bruderj15 <julian.bruder@outlook.com>
2025-03-20 12:25:41 +0100

    examples step...

--- example/charging_station/master/master.ino
+++ example/charging_station/master/master.ino
@@ -2,10 +2,10 @@
 #include <autocharge/Autocharge.hpp>
 
 void start_chg(SlaveData &slave) {
-    Serial.printf("Execute 'start_chg for slave %u'", slave.id);
+    Serial.printf("Execute 'start_chg for slave %u'\n", slave.id);
 }
 void end_chg(SlaveData &slave) {
-    Serial.printf("Execute 'end_chg for slave %u'", slave.id);
+    Serial.printf("Execute 'end_chg for slave %u'\n", slave.id);
 }
 
 auto chargingSlaves = Fifo<SlaveData*>();
@@ -15,11 +15,10 @@ Master master = Master(chargingSlaves,
   end_chg);
 
 void setup() {
-
   master.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(1000);
+  master.step();
 }
