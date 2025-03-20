1ad8b2f6169141e4113c399a95ea5e0f94643634
bruderj15 <julian.bruder@outlook.com>
2025-03-20 12:13:08 +0100

    move singleton-like init to constr

--- src/autocharge/master/Master.hpp
+++ src/autocharge/master/Master.hpp
@@ -17,20 +17,20 @@ public:
       : chargingSlaves(chargingSlaves),
         handleSlaveChargeRequest(handle_slave_charge_request),
         handleSlaveStopChargeRequest(handle_slave_stop_charge_request) {
+    master = this;
   }
-
 
   void begin() override;
   void step();
   void enjoinCharge(SlaveData &slave);
   void cancelCharge(SlaveData &slave);
 
-  protected:
+protected:
+  static Master *master;
 
-    static Master *master;
 private:
   std::unordered_map<uint32_t, SlaveData *> *registeredSlaves =
-      new std::unordered_map<uint32_t, SlaveData*>();
+      new std::unordered_map<uint32_t, SlaveData *>();
   AbstractSet<SlaveData *> &chargingSlaves;
   ChargingStationState chargingStationState = ChargingStationState::OPEN;
   SlaveData *currentChargingSlave = nullptr;
@@ -51,29 +51,29 @@ private:
   void handleExitChargeInfo(SlaveData &slave);
 
   static void onReceiveSingle(uint32_t from, String &message) {
-      Serial.printf("Received single from Node(%u): %s", from, message.c_str());
-      SlaveData *slave = master->registeredSlaves->at(from);
-      if (slave == nullptr) {
-        slave = new SlaveData(from);
-        master->registeredSlaves->insert({from, slave});
-      }
+    Serial.printf("Received single from Node(%u): %s", from, message.c_str());
+    SlaveData *slave = master->registeredSlaves->at(from);
+    if (slave == nullptr) {
+      slave = new SlaveData(from);
+      master->registeredSlaves->insert({from, slave});
+    }
 
-      if (message == "requestCharge") {
-        master->enjoinCharge(*slave);
-      } else if (message == "stopCharge") {
-        master->cancelCharge(*slave);
-      } else if (message == "notifyWork") {
-        slave->state = SlaveState::WORK;
-      } else if (message == "notifyWalkToCharge") {
-        slave->state = SlaveState::WALKING_TO_CHARGE;
-      } else if (message == "notifyWalkInWait") {
-        slave->state = SlaveState::WAIT_CHARGE;
-      } else if (message == "notifyWalkIntoCharge") {
-        slave->state = SlaveState::WALKING_INTO_CHARGE;
-      } else if (message == "notifyInCharge") {
-        slave->state = SlaveState::CHARGE;
-      } else if (message == "notifyExitCharge") {
-        slave->state = SlaveState::EXITING_CHARGE;
-      }
+    if (message == "requestCharge") {
+      master->enjoinCharge(*slave);
+    } else if (message == "stopCharge") {
+      master->cancelCharge(*slave);
+    } else if (message == "notifyWork") {
+      slave->state = SlaveState::WORK;
+    } else if (message == "notifyWalkToCharge") {
+      slave->state = SlaveState::WALKING_TO_CHARGE;
+    } else if (message == "notifyWalkInWait") {
+      slave->state = SlaveState::WAIT_CHARGE;
+    } else if (message == "notifyWalkIntoCharge") {
+      slave->state = SlaveState::WALKING_INTO_CHARGE;
+    } else if (message == "notifyInCharge") {
+      slave->state = SlaveState::CHARGE;
+    } else if (message == "notifyExitCharge") {
+      slave->state = SlaveState::EXITING_CHARGE;
+    }
   }
 };
